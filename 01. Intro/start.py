6cb1a657dbcb4d5bc6f590332763f0bebb1e6baf
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-03-19 16:55:03 +0200

    First lesson

---
+++ 01. Intro/start.py
@@ -0,0 +1,43 @@
+# name = input("What is your name?")
+# print("My name is", name)
+
+
+# int
+# float
+# str
+# bool
+# list
+# tuple
+# dict
+# set complex
+# 123.1232132132121
+
+# a = int(input("Enter First number "))
+# b = int(input("Enter Second number "))
+# # print(type(a))
+# res = a + b
+
+# print("First number ", a, " type ", type(a),
+#       "\n Second number ", b, " type ", type(b))
+
+# print(res)
+
+# if a > b:
+#     print("a > b")
+# elif a < b:
+#     print("a < b")
+# else:
+#     print("a = b")
+
+a = 10
+b = 5
+c = 20
+
+# | or
+# & and
+# >= <= == !=
+
+if a > b and a > c:
+    print("a > b and a > c")
+else:
+    print("Else")
