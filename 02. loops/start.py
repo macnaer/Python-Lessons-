1a602413df2fcafd7b94d6fb7f700e6220a5c9ff
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-03-23 15:30:36 +0200

    Simple loops

---
+++ 02. loops/start.py
@@ -0,0 +1,23 @@
+
+# exit = True
+# while exit:
+#     a = int(input("0. Exit "))
+#     if a == 0:
+#         exit = False
+
+
+# a = int(input("Enter digit: "))
+# i = 1
+# factorial = 1
+# while i < a:
+#     factorial *= i
+#     i += 1
+
+# print("Факторіал числа", a, " = ", factorial)
+
+# count = 10
+
+# for i in range(1, count):
+#     for j in range(1, 10):
+#         print(i * j, end="\t")
+#     print("\n")
