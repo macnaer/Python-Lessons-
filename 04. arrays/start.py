237c46f60c6c87ab1b5ccfbdeb2b71ddd2dc4cfd
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-03-25 16:00:55 +0200

    Lists

---
+++ 04. arrays/start.py
@@ -0,0 +1,68 @@
+import copy
+
+# num = [5, 6, "Seven", 8, 7, True, [1, 55, 100500]]   # List
+
+# # print(num)
+# for item in num:
+#     print(item)
+
+# num[3] = 73
+# num[6][1] = "Середа"
+# num.append(42)
+# num.insert(2, "Heare")
+# num.remove(True)
+
+# # num.sort()
+# print("================================================>")
+
+# for item in num:
+#     print(item)
+
+# print("List length => ", len(num))
+# # print("Min => ", min(num))
+
+# ///////////////////////////
+
+person = ["Tom", "Bob", "Robert", "Tom"]
+
+# for item in person:
+#     print("User => ", item)
+
+# print("#################After##################")
+# person.pop()
+# person.append("Alice")
+# person.sort()
+# print("Count => ", person.count("Tom"))
+
+# for item in person:
+#     print("User => ", item)
+
+person.reverse()
+# for item in person:
+#     print("User => ", item)
+
+
+# userDB = person
+
+# for item in person:
+#     print("person => ", item)
+
+# for item in userDB:
+#     print("userDB => ", item)
+
+# userDB[0] = "Stiven"
+# for item in person:
+#     print("person => ", item)
+
+# for item in userDB:
+#     print("userDB => ", item)
+
+# userDB = copy.deepcopy(person)
+
+# userDB = person[0:2]
+# # userDB[0] = "Stiven"
+# for item in person:
+#     print("person => ", item)
+
+# for item in userDB:
+#     print("userDB => ", item)
