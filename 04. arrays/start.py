8118bb3299b083f0cd8453c8b0322d220366e8aa
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-03-25 17:07:43 +0200

    Task 1

--- 04. arrays/start.py
+++ 04. arrays/start.py
@@ -1,4 +1,5 @@
 import copy
+import random
 
 # num = [5, 6, "Seven", 8, 7, True, [1, 55, 100500]]   # List
 
@@ -66,3 +67,38 @@ person.reverse()
 
 # for item in userDB:
 #     print("userDB => ", item)
+
+
+arr = []
+
+
+def fill_arr(arr):
+    i = 0
+    while i < 7:
+        arr.append(random.randint(-12, 50))
+        i += 1
+
+
+def show_arr(arr):
+    for item in arr:
+        print(item)
+
+
+def find_plus_minus(arr):
+    plus_numbers = 0
+    minus_numbers = 0
+
+    for item in arr:
+        if item > 0:
+            plus_numbers += 1
+        elif item < 0:
+            minus_numbers += 1
+    print("plus_numbers = ", plus_numbers)
+    print("minus_numbers = ", minus_numbers)
+
+
+fill_arr(arr)
+show_arr(arr)
+find_plus_minus(arr)
+
+# print(arr)
