9ca1c242ab0e9870025146aede7b140b82ac8d39
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-04-13 14:18:56 +0300

    start working with sava data into database

--- 11. TelePayBot OOP/lib/db_manager.py
+++ 11. TelePayBot OOP/lib/db_manager.py
@@ -1,39 +1,42 @@
 import mysql.connector
+import requests
 if __name__ == "__main__":
     db_manager
 
 
 class db_manager():
-
     __db = ""
+    __cursor = ""
 
-    def __init__(self, host, user, passwd, database="Test"):
+    def __init__(self, host, user, passwd, database, url):
         self.host = host
         self.user = user
         self.passwd = passwd
         self.database = database
+        self.__URL = url
         self.__db = mysql.connector.connect(
             host=self.host, user=self.user, passwd=self.passwd)
-        cursor = self.__db.cursor()
+        self.__cursor = self.__db.cursor()
 
     def __del__(self):
         self.__db.close()
-    # def connect_to_db():
-    #     db = mysql.connector.connect(
-    #         host="localhost", user="root", passwd="")
-    #     cursor = db.cursor()
-    #     cursor.execute("CREATE DATABASE IF NOT EXISTS CityBuilder;")
-    #     cursor.execute("USE CityBuilder;")
-    #     cursor.execute(
-    #         "CREATE TABLE IF NOT EXISTS capitals (id INT AUTO_INCREMENT PRIMARY KEY, country VARCHAR(255), capital VARCHAR(255), population INT(10), mayor VARCHAR(255))")
 
-    #     return db
+    def get_all_data(self):
+        response = requests.get(self.__URL)
+        return response.json()
 
-    # def add_capital(country, capital, population, mayor):
-    #     db = connect_to_db()
-    #     cursor = db.cursor()
-    #     sql = "INSERT INTO capitals (country,capital,population,mayor ) VALUES (%s,%s,%s,%s)"
-    #     val = (country, capital, population, mayor)
-    #     cursor.execute(sql, val)
-    #     db.commit()
-    #     print(cursor.rowcount, "Capital added")
+    def save_all_data(self, data):
+        print("save_all_data => ", data)
+        self.__cursor.execute("CREATE DATABASE IF NOT EXISTS COVID_19;")
+        self.__cursor.execute("USE COVID_19;")
+
+        #  cursor.execute( "CREATE TABLE IF NOT EXISTS capitals (id INT AUTO_INCREMENT PRIMARY KEY, country VARCHAR(255), capital VARCHAR(255), population INT(10), mayor VARCHAR(255))")
+
+        # def add_capital(country, capital, population, mayor):
+        #     db = connect_to_db()
+        #     cursor = db.cursor()
+        #     sql = "INSERT INTO capitals (country,capital,population,mayor ) VALUES (%s,%s,%s,%s)"
+        #     val = (country, capital, population, mayor)
+        #     cursor.execute(sql, val)
+        #     db.commit()
+        #     print(cursor.rowcount, "Capital added")
--- 11. TelePayBot OOP/start.py
+++ 11. TelePayBot OOP/start.py
@@ -1,4 +1,6 @@
 from lib.db_manager import db_manager
 from lib.settings import *
-
-db_object = db_manager(host, user, passwd, database)
+__URL = "https://api.covid19api.com/summary"
+db_object = db_manager(host, user, passwd, database, __URL)
+covid_19_data = db_object.get_all_data()
+db_object.save_all_data(covid_19_data)
