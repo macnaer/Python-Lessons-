a7d741dff1b409505473f58f8dd35bf80ca21184
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-04-14 12:59:10 +0300

    save_all_data

--- 11. TelePayBot OOP/lib/db_manager.py
+++ 11. TelePayBot OOP/lib/db_manager.py
@@ -5,17 +5,15 @@ if __name__ == "__main__":
 
 
 class db_manager():
-    __db = ""
-    __cursor = ""
 
     def __init__(self, host, user, passwd, database, url):
-        self.host = host
-        self.user = user
-        self.passwd = passwd
-        self.database = database
+        self.__host = host
+        self.__user = user
+        self.__passwd = passwd
+        self.__database = database
         self.__URL = url
         self.__db = mysql.connector.connect(
-            host=self.host, user=self.user, passwd=self.passwd)
+            host=self.__host, user=self.__user, passwd=self.__passwd)
         self.__cursor = self.__db.cursor()
 
     def __del__(self):
@@ -26,17 +24,26 @@ class db_manager():
         return response.json()
 
     def save_all_data(self, data):
-        print("save_all_data => ", data)
+        # print("save_all_data => ", data)
         self.__cursor.execute("CREATE DATABASE IF NOT EXISTS COVID_19;")
         self.__cursor.execute("USE COVID_19;")
+        self.__cursor.execute(
+            "CREATE TABLE IF NOT EXISTS COVID_TABLE (id INT AUTO_INCREMENT PRIMARY KEY, country VARCHAR(255), countryCode VARCHAR(255), Slug VARCHAR(255), NewConfirmed INT(10),TotalConfirmed INT(10),NewDeaths INT(10),TotalDeaths INT(10),NewRecovered INT(10),TotalRecovered INT(10), Date DATETIME)")
 
-        #  cursor.execute( "CREATE TABLE IF NOT EXISTS capitals (id INT AUTO_INCREMENT PRIMARY KEY, country VARCHAR(255), capital VARCHAR(255), population INT(10), mayor VARCHAR(255))")
-
-        # def add_capital(country, capital, population, mayor):
-        #     db = connect_to_db()
-        #     cursor = db.cursor()
-        #     sql = "INSERT INTO capitals (country,capital,population,mayor ) VALUES (%s,%s,%s,%s)"
-        #     val = (country, capital, population, mayor)
-        #     cursor.execute(sql, val)
-        #     db.commit()
-        #     print(cursor.rowcount, "Capital added")
+        for item in data['Countries']:
+            Country = item['Country']
+            CountryCode = item['CountryCode']
+            Slug = item['Slug']
+            NewConfirmed = item['NewConfirmed']
+            TotalConfirmed = item['TotalConfirmed']
+            NewDeaths = item['NewDeaths']
+            TotalDeaths = item['TotalDeaths']
+            NewRecovered = item['NewRecovered']
+            TotalRecovered = item['TotalRecovered']
+            Date = item['Date']
+            sql = "INSERT INTO COVID_TABLE (country,countryCode,Slug,NewConfirmed, TotalConfirmed, NewDeaths,TotalDeaths, NewRecovered, TotalRecovered, Date) VALUES (%s,%s,%s,%s,%s, %s, %s, %s, %s,%s)"
+            val = (Country, CountryCode, Slug, NewConfirmed,
+                   TotalConfirmed, NewDeaths, TotalDeaths, NewRecovered, TotalRecovered, Date)
+            self.__cursor.execute(sql, val)
+            self.__db.commit()
+            print(self.__cursor.rowcount, "Country added")
--- 11. TelePayBot OOP/start.py
+++ 11. TelePayBot OOP/start.py
@@ -2,5 +2,18 @@ from lib.db_manager import db_manager
 from lib.settings import *
 __URL = "https://api.covid19api.com/summary"
 db_object = db_manager(host, user, passwd, database, __URL)
-covid_19_data = db_object.get_all_data()
-db_object.save_all_data(covid_19_data)
+
+
+def menu():
+    exit = False
+    while not exit:
+        choice = int(input("1. Update covid19 database\n0.Exit \n===>"))
+        if choice == 1:
+            covid_19_data = db_object.get_all_data()
+            db_object.save_all_data(covid_19_data)
+        elif choice == 0:
+            exit = True
+            print("Bye...")
+
+
+menu()
